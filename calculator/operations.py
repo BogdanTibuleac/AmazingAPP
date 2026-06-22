91b67ce56be5065aff5eeda69a5ad0e41cea3dd1
Bogdan-Ionut Tibuleac <bogdan-ionut.tibuleac@endava.com>
2026-06-22 14:55:08 +0300

    feat(calculator): implement power operation

--- calculator/operations.py
+++ calculator/operations.py
@@ -4,12 +4,23 @@
 def divide(first_number, second_number):
     """Return the quotient of two numbers."""
     return first_number / second_number
+
+
 def multiply(first_number, second_number):
     """Return the product of two numbers."""
     return first_number * second_number
+
+
 def subtract(first_number, second_number):
     """Return the difference between two numbers."""
     return first_number - second_number
+
+
 def add(first_number, second_number):
     """Return the sum of two numbers."""
     return first_number + second_number
+
+
+def power(base_number, exponent):
+    """Return a number raised to a power."""
+    return base_number**exponent
